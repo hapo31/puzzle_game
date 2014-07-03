5a21b5a9f286460a5ca77824782891e3f54f1591
happou31 <happo31@gmail.com>
2014-07-03 21:47:35 +0900

    少し変更

--- pazzle_game/pazzle_game/Main.cpp
+++ pazzle_game/pazzle_game/Main.cpp
@@ -15,6 +15,17 @@
 
 #include"WindowSize.h"
 
+#include<Effekseer.h>
+#include<EffekseerRendererDX9.h>
+
+#ifdef _DEBUG
+	#pragma comment(lib, "Effekseer.Debug.lib")
+	#pragma comment(lib, "EffekseerRendererDX9.Debug.lib")
+#else
+	#pragma comment(lib, "Effekseer.Release.lib")
+	#pragma comment(lib, "EffekseerRendererDX9.Release.lib")
+#endif
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	SetGraphMode(WindowWidth, WindowHeight, 32);
@@ -43,61 +54,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		if (ProcessMessage() == -1)
 			return 1;
 	}
-
-	////////
-	/*
-	GameObject::field field(7, 7);
-	GameObject::cursor cursor(&field);
-	draw::draw_game g;
-
-	field.initialize();
-
-	while (!CheckHitKey(KEY_INPUT_ESCAPE))
-	{
-		ClearDrawScreen();
-
-		if (kfr[KEY_INPUT_SPACE] == 1)
-			field.initialize();
-		if (kfr[KEY_INPUT_UP] == 1)
-		{
-			cursor.move(GameObject::cursor::UP);
-		}
-		if (kfr[KEY_INPUT_DOWN] == 1)
-		{
-			cursor.move(GameObject::cursor::DOWN);
-		}
-		if (kfr[KEY_INPUT_LEFT] == 1)
-		{
-			cursor.move(GameObject::cursor::LEFT);
-		}
-		if (kfr[KEY_INPUT_RIGHT] == 1)
-		{
-			cursor.move(GameObject::cursor::RIGHT);
-		}
-		if (kfr[KEY_INPUT_Z] == 1)
-		{
-			cursor.rotation(GameObject::cursor::M_DIR::RIGHT);
-		}
-		if (kfr[KEY_INPUT_X] == 1)
-		{
-			cursor.swap(GameObject::cursor::M_DIR::RIGHT);
-		}
-		if (kfr[KEY_INPUT_LSHIFT] == 1)
-		{
-			mode = (mode + 1) % (GameObject::cursor::MODE::ROTA + 1);
-			cursor.set_mode(GameObject::cursor::MODE(mode));
-		}
-
-
-		g.draw_Field(5, 5, field);
-		g.draw_coursor(5, 5, cursor);
-		g.draw_Flags(5, 5, field);
-		g.add_frame();
-		field.update();
-		ScreenFlip();
-		if (ProcessMessage() == -1)
-			return 1;
-	}
-	*/
 	return 0;
 }
